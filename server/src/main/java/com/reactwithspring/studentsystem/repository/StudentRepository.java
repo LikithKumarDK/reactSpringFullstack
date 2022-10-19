7f3d628db07776e122ee183a951357123d926825
LikithKumarDK <likithcse168@gmail.com>
2022-10-19 11:18:25 +0530

    Update: server base code

---
+++ server/src/main/java/com/reactwithspring/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.reactwithspring.studentsystem.repository;
+
+import com.reactwithspring.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
